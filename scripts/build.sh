fb5983125a3e0f446b3c832740c6b9d93fdffae9
Rick <ruanpaiolinho060809@gmail.com>
2026-05-27 23:55:04 -0300

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -13,22 +13,36 @@ export KBUILD_BUILD_HOST=actions
 export CC=clang
 export LD=ld.lld
 
+export CLANG_TRIPLE=aarch64-linux-gnu-
+export CROSS_COMPILE=aarch64-linux-gnu-
+export CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+
 mkdir -p out
 
 echo "USING GKI DEFCONFIG"
 
-make O=out gki_defconfig
+make \
+    O=out \
+    ARCH=arm64 \
+    gki_defconfig
 
 cat ../config/droidspaces.fragment >> out/.config
 
-make O=out olddefconfig
+make \
+    O=out \
+    ARCH=arm64 \
+    olddefconfig
 
 make -j$(nproc --all) \
     O=out \
+    ARCH=arm64 \
     CC=clang \
     LD=ld.lld \
     LLVM=1 \
     LLVM_IAS=1 \
+    CLANG_TRIPLE=aarch64-linux-gnu- \
+    CROSS_COMPILE=aarch64-linux-gnu- \
+    CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
     HOSTCC=gcc \
     HOSTCXX=g++ \
     KCFLAGS="-Wno-error"
